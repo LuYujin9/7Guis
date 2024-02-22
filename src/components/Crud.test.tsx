593b30b46647ead46f5cbe15cc97ad744e178869
盧昱錦 <luyujin@roakirakins-MBP.fritz.box>
2024-02-22 12:05:43 +0100

    last change before storage as stash in a new branch

---
+++ src/components/Crud.test.tsx
@@ -0,0 +1,33 @@
+import { filterUserList } from "./CrudTwo";
+import { expect, test } from "vitest";
+
+test("filterUserList returns a filtered name list, case-insensitive", () => {
+  const userList = [
+    { name: "Jane", surname: "Davis", id: "0" },
+    { name: "John", surname: "Wilson", id: "1" },
+    { name: "Jack", surname: "Roman", id: "2" },
+    { name: "Isabella", surname: "White", id: "3" },
+    { name: "Jane", surname: "Davis", id: "4" },
+    { name: "John", surname: "wilson", id: "5" },
+    { name: "Jack", surname: "Roman", id: "6" },
+    { name: "Isabella", surname: "white", id: "7" },
+  ];
+  const filteredUserListOne = filterUserList("w", userList);
+  const filteredUserListTwo = filterUserList("W", userList);
+
+  expect(filteredUserListOne).toStrictEqual([
+    { name: "John", surname: "Wilson", id: "1" },
+    { name: "Isabella", surname: "White", id: "3" },
+    { name: "John", surname: "wilson", id: "5" },
+    { name: "Isabella", surname: "white", id: "7" },
+  ]);
+  expect(filteredUserListOne).toStrictEqual(filteredUserListTwo);
+
+  const filteredUserListThree = filterUserList("wH", userList);
+  const filteredUserListFour = filterUserList("Wh", userList);
+  expect(filteredUserListThree).toStrictEqual([
+    { name: "Isabella", surname: "White", id: "3" },
+    { name: "Isabella", surname: "white", id: "7" },
+  ]);
+  expect(filteredUserListThree).toStrictEqual(filteredUserListFour);
+});
